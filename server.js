77d9b16484554861f48e866b12a575958bb09fec
Diego <diegotozetto@gmail.com>
2021-03-27 19:40:16 -0300

    Atualiza para deploy

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const favicon = require('express-favicon');
+const path = require('path');
+const app = express();
+
+app.use(favicon(__dirname + '/dist/favicon.ico'));
+app.use(express.static(__dirname));
+app.use(express.static(path.join(__dirname, 'dist')));
+
+app.get('/*', function (req, res) {
+  res.sendFile(path.join(__dirname, 'dist', 'index.html'));
+});
+
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+  console.log("Servidor Rodando...");
+});
